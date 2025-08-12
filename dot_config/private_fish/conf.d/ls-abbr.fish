15cb0361739e5fa96d85b67b685a74773dc81e28
Théophile Cailliau <tjbcg@pm.me>
2025-08-12 11:29:23 +0200

    Update .config/fish/conf.d/ls-abbr.fish

--- dot_config/private_fish/conf.d/ls-abbr.fish
+++ dot_config/private_fish/conf.d/ls-abbr.fish
@@ -1,3 +1,4 @@
-abbr --add l ls
-abbr --add la ls -la
-abbr --add lah ls -lah
+type -q eza; and abbr --add l eza; or abbr --add l ls
+type -q eza; and abbr --add la eza -la; or abbr --add la ls -la
+type -q eza; and abbr --add lah eza -lah; or abbr --add lah ls -lah
+type -q eza; and abbr --add ls eza
