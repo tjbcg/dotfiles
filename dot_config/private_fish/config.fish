680c6c3404dcdbadbfefbe8b791a52b60560a601
Théophile Cailliau <tjbcg@pm.me>
2025-08-12 11:29:42 +0200

    Update .config/fish/config.fish Update .config/starship.toml

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,4 +2,15 @@ if status is-interactive
     type -q starship; and starship init fish | source
     type -q pyenv; and pyenv init - | source
     type -q keychain; and eval (keychain --eval --quiet);
+    # Only run this in interactive shells
+
+    set fish_cursor_default     block      blink
+    set fish_cursor_insert      line       blink
+    set fish_cursor_replace_one underscore blink
+    set fish_cursor_visual      block
+
+    function fish_user_key_bindings
+      fish_default_key_bindings -M insert
+      fish_vi_key_bindings --no-erase insert
+    end
 end
